b4c3cf8d1259465c23cebfc843fd70aa7f2f344f
ogata <Shin.Ogata@jp.sony.com>
2015-04-09 10:10:13 +0900

    added: menu view and dummy auto-test contents.

---
+++ dev/app/scripts/View/Menu.ts
@@ -0,0 +1,35 @@
+﻿/// <reference path="../../modules/include/frameworks.d.ts" />
+
+module NativeBridgeDevBed {
+	export module View {
+
+		import Framework = CDP.Framework;
+		import UI = CDP.UI;
+
+		var TAG: string = "[NativeBridgeDevBed.View.Menu] ";
+
+		/**
+		 * @class Menu
+		 * @brief Menu View クラス
+		 */
+		class Menu extends UI.PageView<Backbone.Model> {
+
+			/**
+			 * constructor
+			 */
+			constructor() {
+				super("/templates/menu.html", "page-devbed-menu", { route: "menu" });
+			}
+
+			///////////////////////////////////////////////////////////////////////
+			// Override: UI.PageView
+
+			//! jQM event: "pagebeforeshow" に対応
+			onInitialize(event: JQueryEventObject): void {
+				super.onInitialize(event);
+			}
+		}
+
+		var menuView = new Menu();
+	}
+}
