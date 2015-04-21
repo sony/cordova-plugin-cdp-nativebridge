8335f78db52ecbd8aedae2026b9e6ed746bbc278
Shin Ogata <Shin.Ogata@jp.sony.com>
2015-04-21 09:32:23 +0900

    fixed: plugin definitions.

--- dev/platforms/ios/NativeBridgeDevBed/Plugins/CNPNativeBridge/CNPNativeBridge.h
+++ dev/platforms/ios/NativeBridgeDevBed/Plugins/CNPNativeBridge/CNPNativeBridge.h
@@ -1,4 +1,34 @@
 /*
- * CNPNativeBridge.h
- * Interface definition file for CDP NativeBridge class.
+ * @file CNPNativeBridge.h
+ * @brief Interface definition file for CDP NativeBridge class.
  */
+
+#import <Cordova/CDV.h>
+
+@interface CNPNativeBridge : CDVPlugin
+
+//////////////////////////////////////////////////////
+// Plugin I/F
+
+/**
+ * standard execute task method entry.
+ *
+ * @param command [in] cordova invoked command pointer.
+ */
+- (void) execTask:  (CDVInvokedUrlCommand*)command;
+
+/**
+ * cancel task entry.
+ *
+ * @param command [in] cordova invoked command pointer.
+ */
+- (void) cancelTask:  (CDVInvokedUrlCommand*)command;
+
+/**
+ * dispose task entry.
+ *
+ * @param command [in] cordova invoked command pointer.
+ */
+- (void) disposeTask:  (CDVInvokedUrlCommand*)command;
+
+@end
--- dev/platforms/ios/NativeBridgeDevBed/Plugins/CNPNativeBridge/CNPNativeBridge.m
+++ dev/platforms/ios/NativeBridgeDevBed/Plugins/CNPNativeBridge/CNPNativeBridge.m
@@ -1,4 +1,29 @@
-/*
- * CNPNativeBridge.m
- * Implementation file for CDP NativeBridge class.
+/**
+ * @file CNPNativeBridge.m
+ * @brief Implementation file for CDP NativeBridge class.
  */
+
+#import "CNPNativeBridge.h"
+
+@implementation CNPNativeBridge
+
+//////////////////////////////////////////////////////
+// Plugin I/F
+
+- (void) execTask:(CDVInvokedUrlCommand *)command
+{
+    // TODO:
+    NSLog(@"execTask called.");
+}
+
+- (void) cancelTask:(CDVInvokedUrlCommand *)command
+{
+    NSLog(@"cancelTask called.");
+}
+
+- (void) disposeTask:(CDVInvokedUrlCommand *)command
+{
+    NSLog(@"disposeTask called.");
+}
+
+@end
