02a1fa0914382ff260f15b2c8520ea17834ce506
ogata <Shin.Ogata@jp.sony.com>
2015-04-27 18:28:10 +0900

    implemented: deploy to release directory.

--- dev/build/tasks/build.dev.bed.js
+++ dev/build/tasks/build.dev.bed.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
             devbed_deploy: {
                 files: {
                     src: [
-                        '../www/*', '../src/*', '../tests/*', '../dist/**'
+                        '../www/*', '../src/*', '../tests/*', '../dist/**', '../release/**'
                     ],
                 },
             },
@@ -81,11 +81,85 @@ module.exports = function (grunt) {
                         src: ['cdp.tools-*.js', 'cdp.tools.js'],
                         dest: '../dist/external',
                     },
+                    {// external: cdp.plugin.nativebridge.d.ts
+                        expand: true,
+                        cwd: '../www',
+                        src: ['cdp.plugin.nativebridge.d.ts'],
+                        dest: '../dist/external/include',
+                    },
                     {// external: d.ts
                         expand: true,
                         cwd: '<%= orgsrc %>/<%= modules %>/include',
-                        src: ['jquery.d.ts', 'underscore.d.ts', 'cdp.tools.d.ts'],
-                        dest: '../dist/include',
+                        src: ['jquery.d.ts', 'underscore.d.ts', 'cdp.tools.d.ts', 'require.d.ts', 'cordova.d.ts', 'hogan.d.ts', 'plugins/*.d.ts'],
+                        dest: '../dist/external/include',
+                    },
+                ],
+            },
+            devbed_release: {
+                files: [
+                    {// cdp.plugin.nativebridge.
+                        expand: true,
+                        cwd: '../www',
+                        src: ['**'],
+                        dest: '../release/plugins/com.sony.cdp.plugin.nativebridge/www',
+                    },
+                    {// cdp.plugin.nativebridge native source files
+                        expand: true,
+                        cwd: '../src',
+                        src: ['**'],
+                        dest: '../release/plugins/com.sony.cdp.plugin.nativebridge/src',
+                    },
+                    {// cdp.plugin.nativebridge plugin.xml
+                        expand: true,
+                        cwd: '../',
+                        src: ['plugin.xml'],
+                        dest: '../release/plugins/com.sony.cdp.plugin.nativebridge',
+                    },
+                    {// cdp.plugin.nativebridge.tests
+                        expand: true,
+                        cwd: '../tests',
+                        src: ['**'],
+                        dest: '../release/plugins/com.sony.cdp.plugin.nativebridge/tests',
+                    },
+                    {// cdp.nativebridge
+                        expand: true,
+                        cwd: '../dist',
+                        src: ['*.js'],
+                        dest: '../release/modules/sony/cdp/scripts',
+                    },
+                    {// cdp.nativebridge d.ts
+                        expand: true,
+                        cwd: '../dist/include',
+                        src: ['cdp.nativebridge.d.ts'],
+                        dest: '../release/modules/include',
+                    },
+                ],
+            },
+            devbed_release_external: { // TBD
+                files: [
+                    {// jquery
+                        expand: true,
+                        cwd: '../dist/external',
+                        src: ['jquery*.js'],
+                        dest: '../release/modules/jquery/scripts',
+                    },
+                    {// underscore
+                        expand: true,
+                        cwd: '../dist/external',
+                        src: ['underscore*.js'],
+                        dest: '../release/modules/underscore/scripts',
+                    },
+                    {// cdp.tools
+                        expand: true,
+                        cwd: '../dist/external',
+                        src: ['cdp.tools*.js'],
+                        dest: '../release/modules/sony/cdp/scripts',
+                    },
+                    {// d.ts
+                        expand: true,
+                        cwd: '../dist/external/include',
+                        src: ['**'],
+                        dest: '../release/modules/include',
                     },
                 ],
             },
@@ -96,5 +170,11 @@ module.exports = function (grunt) {
     //__________________________________________________________________________________________________________________________________________________________________________________________//
 
 
-    grunt.registerTask('deploy', ['clean:devbed_deploy', 'module', 'copy:devbed_deploy', 'copy:devbed_deploy_external', /*'clean:tmpdir'*/]);
+    grunt.registerTask('deploy', [
+        'clean:devbed_deploy',
+        'module',
+        'copy:devbed_deploy', 'copy:devbed_deploy_external',
+        'copy:devbed_release', 'copy:devbed_release_external',
+        'clean:tmpdir'
+    ]);
 };
