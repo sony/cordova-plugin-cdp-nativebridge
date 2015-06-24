00aee3033f6de47d4ce06a5cfa2edcc090bfec05
ogata <Shin.Ogata@jp.sony.com>
2015-06-24 14:31:01 +0900

    add new test-framework for android.

--- dev/platforms/android/assets/www/cordova_plugins.js
+++ dev/platforms/android/assets/www/cordova_plugins.js
@@ -10,13 +10,30 @@ module.exports = [
         "clobbers": [
             "CDP.Plugin.NativeBridge"
         ]
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/tests.js",
+        "id": "com.sony.cdp.plugin.test-framework.cdvtests"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/jasmine_helpers.js",
+        "id": "com.sony.cdp.plugin.test-framework.jasmine_helpers"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/medic.js",
+        "id": "com.sony.cdp.plugin.test-framework.medic"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/main.js",
+        "id": "com.sony.cdp.plugin.test-framework.main"
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.sony.cdp.plugin.nativebridge.tests": "1.0.0",
-    "com.sony.cdp.plugin.nativebridge": "1.0.0"
+    "com.sony.cdp.plugin.nativebridge": "1.0.0",
+    "com.sony.cdp.plugin.test-framework": "1.0.1-dev-cdp"
 }
 // BOTTOM OF METADATA
 });
