e4e353ed5e1b6c4ade7db1f9670b3136db3c4acd
Shin-Ogata <Shin.Ogata@jp.sony.com>
2016-01-05 15:01:19 +0900

    updated: cordova-cli 5.4.1 (for android)

--- dev/platforms/android/assets/www/cordova_plugins.js
+++ dev/platforms/android/assets/www/cordova_plugins.js
@@ -2,38 +2,44 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
         "file": "plugins/com.sony.cdp.plugin.nativebridge.tests/www/tests.js",
-        "id": "com.sony.cdp.plugin.nativebridge.tests.tests"
+        "id": "com.sony.cdp.plugin.nativebridge.tests.tests",
+        "pluginId": "com.sony.cdp.plugin.nativebridge.tests"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/tests.js",
+        "id": "com.sony.cdp.plugin.test-framework.cdvtests",
+        "pluginId": "com.sony.cdp.plugin.test-framework"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/jasmine_helpers.js",
+        "id": "com.sony.cdp.plugin.test-framework.jasmine_helpers",
+        "pluginId": "com.sony.cdp.plugin.test-framework"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/medic.js",
+        "id": "com.sony.cdp.plugin.test-framework.medic",
+        "pluginId": "com.sony.cdp.plugin.test-framework"
+    },
+    {
+        "file": "plugins/com.sony.cdp.plugin.test-framework/www/main.js",
+        "id": "com.sony.cdp.plugin.test-framework.main",
+        "pluginId": "com.sony.cdp.plugin.test-framework"
     },
     {
         "file": "plugins/cordova-plugin-cdp-nativebridge/www/cdp.plugin.nativebridge.js",
         "id": "cordova-plugin-cdp-nativebridge.NativeBridge",
+        "pluginId": "cordova-plugin-cdp-nativebridge",
         "clobbers": [
             "CDP.Plugin.NativeBridge"
         ]
-    },
-    {
-        "file": "plugins/com.sony.cdp.plugin.test-framework/www/tests.js",
-        "id": "com.sony.cdp.plugin.test-framework.cdvtests"
-    },
-    {
-        "file": "plugins/com.sony.cdp.plugin.test-framework/www/jasmine_helpers.js",
-        "id": "com.sony.cdp.plugin.test-framework.jasmine_helpers"
-    },
-    {
-        "file": "plugins/com.sony.cdp.plugin.test-framework/www/medic.js",
-        "id": "com.sony.cdp.plugin.test-framework.medic"
-    },
-    {
-        "file": "plugins/com.sony.cdp.plugin.test-framework/www/main.js",
-        "id": "com.sony.cdp.plugin.test-framework.main"
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.sony.cdp.plugin.nativebridge.tests": "1.0.1",
-    "cordova-plugin-cdp-nativebridge": "1.1.0-dev",
-    "com.sony.cdp.plugin.test-framework": "1.0.1-dev-cdp"
+    "com.sony.cdp.plugin.test-framework": "1.0.1-dev-cdp",
+    "cordova-plugin-cdp-nativebridge": "1.1.0-dev"
 }
 // BOTTOM OF METADATA
 });
